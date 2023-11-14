d0471b7362f3f97b2ec5c7044b3b0fc42927f581
onneoul <kimminse99@naver.com>
2023-11-15 01:19:44 +0900

    Exhibition version

--- Python/FlaskApp/app/app.py
+++ Python/FlaskApp/app/app.py
@@ -549,5 +549,5 @@ def download(filename):
     return send_file(filename, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    # serve(app, host='0.0.0.0', port=5000)
+    # app.run(debug=True)
+    serve(app, host='0.0.0.0', port=5000)
--- README.md
+++
@@ -1,3 +0,0 @@
-# QASupportSystem
-A web-based dashboard to streamline the game QA process: 
-A program that allows you to view play data, logs, bug reports, and screenshots at a glance
